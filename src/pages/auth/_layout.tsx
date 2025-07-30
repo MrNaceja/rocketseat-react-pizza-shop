fc6253155cc0b0cb3d58dbe093aaea5af0986e28
Eduardo Toriani <edutoriani13@gmail.com>
2025-07-29 22:58:24 -0300

    feat: create sign-in page

--- src/pages/auth/_layout.tsx
+++ src/pages/auth/_layout.tsx
@@ -1,10 +1,21 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Layout Auth</h1>
-      <Outlet />
-    </div>
+    <section className="grid min-h-screen grid-cols-2">
+      <div className="bg-secondary border-r-foreground/5 flex h-full min-w-3/4 flex-col justify-between border-r p-6">
+        <header className="flex items-center gap-3">
+          <Pizza className="size-8" />
+          <h1 className="text-foreground text-lg font-semibold">Pizza Shop</h1>
+        </header>
+        <footer className="text-muted-foreground text-sm">
+          Painel do parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <main>
+        <Outlet />
+      </main>
+    </section>
   )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,68 @@
 import { useHead } from '@unhead/react'
+import { useCallback } from 'react'
+import { type SubmitHandler, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import z4 from 'zod/v4'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInFormSchema = z4.object({
+  email: z4.email(),
+})
+
+type SignInForm = z4.infer<typeof signInFormSchema>
 
 export function SignInPage() {
   useHead({
     title: 'Sign In',
   })
-  return <h1>Sign In</h1>
+
+  const signInForm = useForm<SignInForm>({
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  const handleAccessPainel = useCallback<SubmitHandler<SignInForm>>(
+    async ({ email }) => {
+      toast.success(`Link mágico enviado para ${email}.`)
+    },
+    [],
+  )
+
+  return (
+    <div className="grid h-full place-items-center p-5">
+      <div className="flex h-full w-sm flex-col items-center justify-center gap-6">
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-foreground text-3xl font-bold tracking-tight">
+            Acessar Painel
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
+        </div>
+
+        <form
+          className="flex w-full flex-col gap-4"
+          onSubmit={signInForm.handleSubmit(handleAccessPainel)}
+        >
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="email">Seu email</Label>
+            <Input
+              placeholder="example@email.com"
+              id="email"
+              type="email"
+              {...signInForm.register('email')}
+            />
+          </div>
+
+          <Button type="submit" disabled={signInForm.formState.isSubmitting}>
+            Acessar Painel
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
 }
