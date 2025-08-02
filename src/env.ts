8d1998f4f77325b9a86460ee6e4a7e83e342655b
Eduardo Toriani <edutoriani13@gmail.com>
2025-08-01 23:01:24 -0300

    fix: fix pizza shop api url env name

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import z4 from 'zod/v4'
 
 const envSchema = z4.object({
-  VITE_PIZZA_SHOP_URL: z4.url(),
+  VITE_PIZZA_SHOP_API_URL: z4.url(),
 })
 
 export type Env = z4.infer<typeof envSchema>
--- src/services/pizza-shop/api.ts
+++ src/services/pizza-shop/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 import { env } from '@/env'
 
 export const api = axios.create({
-  baseURL: env.VITE_PIZZA_SHOP_URL,
+  baseURL: env.VITE_PIZZA_SHOP_API_URL,
 })
