e9dcf59a7fb3ae966c2199c10dbf0edec67e2c04
Eduardo Toriani <edutoriani13@gmail.com>
2025-08-01 22:18:20 -0300

    chore: init api client using env

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+import z4 from 'zod/v4'
+
+const envSchema = z4.object({
+  VITE_PIZZA_SHOP_URL: z4.url(),
+})
+
+export type Env = z4.infer<typeof envSchema>
+
+export const env = envSchema.parse(import.meta.env)
