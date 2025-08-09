7dd25f65d6e59d61bc44d8252539a75de47ec112
Eduardo Toriani <edutoriani13@gmail.com>
2025-08-08 22:36:11 -0300

    test: setup test enviroment

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createHead, UnheadProvider } from '@unhead/react/client'
-import type { PropsWithChildren } from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { Route } from 'react-router'
 
-import { Toaster } from '@/components/sonner'
 import { AppLayout } from '@/pages/app/_layout'
 import { DashboardPage } from '@/pages/app/dashboard'
 import { OrdersPage } from '@/pages/app/orders'
 import { AuthLayout } from '@/pages/auth/_layout'
 import { SignInPage } from '@/pages/auth/sign-in'
 import { SignUpPage } from '@/pages/auth/sign-up'
-import { ErrorBoundaryPage } from '@/pages/error-boundary'
 import { NotFoundPage } from '@/pages/not-found'
-import { ThemeProvider } from '@/providers/theme/provider'
+
+import { ErrorBoundaryPage } from "@/pages/error-boundary"
+import { ThemeProvider } from "@/providers/theme/provider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { createHead, UnheadProvider } from "@unhead/react/client"
+import type { PropsWithChildren } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+import { BrowserRouter, Routes } from "react-router"
+import { Toaster } from "sonner"
 
 const head = createHead({
   init: [
@@ -24,6 +26,11 @@ const head = createHead({
 })
 
 const queryClient = new QueryClient()
+
+export const appProvidersDependencies = {
+  head, 
+  queryClient
+}
 
 export function App() {
   return (
@@ -57,3 +64,5 @@ function Providers({ children }: PropsWithChildren) {
     </UnheadProvider>
   )
 }
+
+
